96d490f07b89c73f95ae6a434434ec80f3a06996
Programmer And Web Developer <webdeveloper.adhg.2024@gmail.com>
2024-09-14 18:44:14 -0600

    Urls y Rutas ya definidas. (Faltan las Rutas POST)

---
+++ NEBULOUS/Controllers/Routs/Post.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NEBULOUS.Controllers.Urls;
+
+namespace NEBULOUS.Controllers.Routs
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Post : ControllerBase
+    {
+        // auth-session
+        [HttpGet(Urls.Urls.AuthSession)]
+        public IActionResult authSession()
+        {
+            bool res = false;
+            if (res)
+            {
+                res = true;
+                return Ok(res);
+            }
+            else
+            {
+                //return Ok(res);
+                return Ok("Ruta Auth-Session");
+            }
+        }
+       
+        // logout
+        [HttpGet(Urls.Urls.Logout)]
+        public IActionResult logOut()
+        {
+            bool res = false;
+            if (res)
+            {
+                res = true;
+                return Ok(res);
+            }
+            else
+            {
+                //return Ok(res);
+                return Ok("Ruta Log-Out");
+            }
+        }
+    }
+}
---
+++ NEBULOUS/Controllers/Urls/Urls.cs
@@ -0,0 +1,25 @@
+﻿namespace NEBULOUS.Controllers.Urls
+{
+    public class Urls
+    {
+        public const string Root = "/";
+        public const string Login = "/log-in";
+        public const string Logout = "/log-out";
+        public const string AuthSession = "/auth-session";
+        public const string Dashboard = "/dashboard";
+        public const string Suppliers = "/suppliers";
+        public const string Products = "/products";
+        public const string ProductCategory = "/product-category";
+        public const string ProductSubCategory = "/product-sub-category";
+        public const string ProductBrands = "/product-brands";
+
+        public const string Inventory = "/inventory";
+        public const string GeneralBalance = "/general-balance";
+
+        public static class Operations
+        {
+            public const string Buys = "/product-buys";
+            public const string Sale = "/product-sale";
+        }
+    }
+}
