4e12e4ecbf2515f940e53d871f22cf94b3c37246
Programmer And Web Developer <webdeveloper.adhg.2024@gmail.com>
2024-09-23 16:43:32 -0600

    Corrección de scripts de bd. Proceso de configuración de ruta de Login.

---
+++ NEBULOUS/Controllers/Routs/Get/Login/Login.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NEBULOUS.Controllers.Routs.Get.Login
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Login : ControllerBase
+    {
+        // login
+        [HttpGet(Urls.Urls.Login)]
+        public IActionResult login()
+        {
+            bool res = false;
+            if (res)
+            {
+                res = true;
+                return Ok(res);
+            }
+            else
+            {
+                //return Ok(res);
+                return Ok("Ruta Login");
+            }
+        }
+    }
+}
---
+++ NEBULOUS/Controllers/Routs/Post/Login/Login.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NEBULOUS.Controllers.Routs.Post.Login
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Login : ControllerBase
+    {
+        // login
+        [HttpPost(Urls.Urls.AuthSession)]
+        public IActionResult authSession()
+        {
+            return Ok();
+        }
+    }
+}
