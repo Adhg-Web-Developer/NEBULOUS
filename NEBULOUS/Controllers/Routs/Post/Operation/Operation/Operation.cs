fe21e967b119a415f4c13711448b809147d6de18
Luis Jared <luis.jared945@gmail.com>
2024-10-10 16:19:17 -0600

    Error corregido en la carpeta lógica de Operation.

--- NEBULOUS/Controllers/Routs/Post/Operation/Operation/Operation.cs
+++ NEBULOUS/Controllers/Routs/Post/Operation/Operation/Operation.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using NEBULOUS.Models.Operation.OperationDetails;
+using NEBULOUS.Logic.Operation.Operation;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
